9c2ed5d249340c70ffec82b95831673e57f2eacf
Roman Chvanikov <chvanikoff@gmail.com>
2015-11-21 17:40:53 +0300

    replaced timestamp-based IDs with phash2(make_ref)

--- lib/apns/message.ex
+++ lib/apns/message.ex
@@ -13,11 +13,9 @@ defmodule APNS.Message do
   ]
 
   def new do
-    {_, _, ms} = :os.timestamp
-    s = :calendar.datetime_to_gregorian_seconds :calendar.universal_time
-    f = rem s, 65536
-    l = rem ms, 65536
-    new <<f :: 8-unsigned-integer-unit(2), l :: 8-unsigned-integer-unit(2)>>
+    make_ref
+    |> :erlang.phash2
+    |> new
   end
   def new(id), do: %__MODULE__{id: id}
 
