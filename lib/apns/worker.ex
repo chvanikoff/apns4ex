3301c005adc283472b65211c6db6e2ed652f1645
Vladimir Reshetnikov <zepplock@vova.org>
2016-02-04 18:54:42 -0800

    Pause the worker for 'timeout' in case the host is unreachable

--- lib/apns/worker.ex
+++ lib/apns/worker.ex
@@ -62,6 +62,7 @@ defmodule APNS.Worker do
         {:noreply, %{state | socket_apple: socket, counter: 0}}
       {:error, reason} ->
         Logger.error "[APNS] failed to connect #{address}, reason given: #{inspect reason}"
+        :timer.sleep(timeout)
         {:stop, {:connection_failed, address}, state}
     end
   end
@@ -79,6 +80,7 @@ defmodule APNS.Worker do
         {:noreply, %{state | socket_feedback: socket}}
       {:error, reason} ->
         Logger.error "[APNS] failed to connect #{address}, reason given: #{inspect reason}"
+        :timer.sleep(timeout)
         {:stop, {:connection_failed, address}, state}
     end
   end
