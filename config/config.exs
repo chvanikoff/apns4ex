f2156cdef3945da66737e458ed92ff430e96099c
Linus Pettersson and Nicklas Ramhöj <dev+linus+nicklas@elabs.se>
2016-03-23 10:14:26 +0100

    Simplify cert setup when contributing

--- config/config.exs
+++ config/config.exs
@@ -7,28 +7,10 @@ config :apns,
   reconnect_after:  1000,
   support_old_ios:  true,
   pools: [
-    app1_dev_pool: [
+    test: [
       env: :dev,
-      certfile: "/path/to/app1_dev.pem",
+      certfile: {:apns, "certs/dev.pem"},
       pool_size: 10,
       pool_max_overflow: 5
-    ],
-    app1_prod_pool: [
-      env: :prod,
-      certfile: "/path/to/app1_prod.pem",
-      pool_size: 100,
-      pool_max_overflow: 50
-    ],
-    app2_dev_pool: [
-      env: :dev,
-      certfile: "/path/to/app2_dev.pem",
-      pool_size: 10,
-      pool_max_overflow: 5
-    ],
-    app2_prod_pool: [
-      env: :prod,
-      certfile: "/path/to/app12prod.pem",
-      pool_size: 100,
-      pool_max_overflow: 50
     ]
   ]
