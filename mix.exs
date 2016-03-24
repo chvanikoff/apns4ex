7410100f5358e89d2d3d3accb50a63fdb29ac2ea
Vladimir Reshetnikov <zepplock@vova.org>
2016-03-23 17:31:36 -0700

    Version 0.0.13 update

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule APNS.Mixfile do
   def project do
     [
       app: :apns,
-      version: "0.0.12",
+      version: "0.0.13",
       elixir: "~> 1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
