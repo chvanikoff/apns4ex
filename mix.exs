8bf5632ad687c5c7fb87d994ab14762d4ee698c9
Vladimir Reshetnikov <zepplock@users.noreply.github.com>
2016-08-11 19:59:25 -0700

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule APNS.Mixfile do
   def project do
     [
       app: :apns,
-      version: "0.9.3",
+      version: "0.9.4",
       elixir: "~> 1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
